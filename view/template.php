4aba528a0a304eb4bcb3716b2e12c5dca66b9ebc
Nassim <enaciri.nassim@gmail.com>
2024-04-15 01:54:18 +0200

    start front

--- view/template.php
+++ view/template.php
@@ -5,34 +5,98 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filmopédia</title>
+    <link rel="stylesheet" href="./public/css/style.css">
 </head>
 
 <body>
-    <header>
-        <nav>
-            <ul>
+    <div class="index">
+        <!-- HEADER -->
+        <header>
+            <p class="logo">
+                Filmopedia
+            </p>
+            <ul class="navbar">
                 <li>
-                    <a href="index.php?action=home">Home</a>
+                    <a href="">
+
+                        HOME
+                    </a>
                 </li>
                 <li>
-                    <a href="index.php?action=actor">Actor</a>
+                    <a href="">
+
+                        GENRE
+                    </a>
                 </li>
                 <li>
-                    <a href="index.php?action=movie">Movie</a>
+                    <a href="">
+
+                        BROWSE
+                    </a>
                 </li>
                 <li>
-                    <a href="index.php?action=director"> Director </a>
+                    <a href="">
+
+                        TOP
+                    </a>
                 </li>
             </ul>
-        </nav>
-    </header>
+            <img class="admin-logo" src="./public/img/svg/adminLogo1_x2.svg" />
+        </header>
 
-    <?= $content ?>
+        <!-- SEARCHBAR SECTION -->
+        <div>
+            <article class="searchbar-background">
+                <form class="searchbar-section" action="" method="get">
+                    <input id="search" type="text" placeholder="Search a movie, an actor or a director">
+                    <input id="send" type="submit" value="Search">
+                </form>
+            </article>
+        </div>
 
-    <footer>
-        <p>Filmopédia</p>
-    </footer>
+        <!-- GENRE SECTION -->
+        <article class="section-2">
+            <div class="choose-agenre">
+                Choose a genre
+            </div>
+            <div class="genre-list">
+                <div class="genre-box-3">
+                    <img class="arrow-2" src="./public/img/svg/arrow2_x2.svg" />
+                    <div class="action">
+                        Action
+                    </div>
+                </div>
+                <div class="genre-box-2">
+                    <span class="horror">
+                        Horror
+                    </span>
+                </div>
+                <div class="genre-box-1">
+                    <span class="comedy">
+                        Comedy
+                    </span>
+                </div>
+                <div class="genre-box">
+                    <span class="fantasy">
+                        Fantasy<br />
 
+                    </span>
+                    <img class="arrow-1" src="./public/img/svg/arrow1_x2.svg" />
+                </div>
+                <div class="rectangle-7">
+                </div>
+            </div>
+            <div class="line-1">
+            </div>
+        </article>
+
+        <!-- FOOTER -->
+        <div class="footer">
+            <span class="filmopedia">
+                2024 © Filmopedia
+            </span>
+        </div>
+    </div>
 
 </body>
 
